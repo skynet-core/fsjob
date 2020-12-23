8a263cf7cb1501aae0f080aa54db95ea497cf555
Skynet Core <skynetvasyl@gmail.com>
2020-12-23 09:42:12 +0200

    pass correct file name

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,19 @@ trap "exit 1" SIGINT SIGTERM
 # WATCH_TARGET
 if [ -z $WATCH_TARGET ]; then
     WATCH_TARGET="$(pwd)"
+elif [ -d "$WATCH_TARGET" ]; then
+    WATCH_TARGET=$(
+        cd $WATCH_TARGET
+        pwd
+    )
+else
+    dir_name=$(dirname $WATCH_TARGET)
+    file_name="${WATCH_TARGET##*/}"
+    full_path=$(
+        cd $dir_name
+        pwd
+    )
+    WATCH_TARGET="$full_name/$file_name"
 fi
 
 if [ -z $WAIT_EVENTS ]; then
@@ -43,18 +56,14 @@ EOF
 
 while true; do
     debug "INFO: wait for $WAIT_EVENTS on $WATCH_TARGET"
-    name=$(inotifywait -r -e $WAIT_EVENTS $WATCH_TARGET 2>/dev/null | cut -d ' ' -f3)
+    name=$(inotifywait -r -e $WAIT_EVENTS $WATCH_TARGET --format '%w/%f' 2>/dev/null)
     debug "INFO: got event on $name"
 
     for job in $(find "$JOBS_FOLDER" -type f -iname "*.job" | sort); do
         debug "INFO: process $job"
-        param="$WATCH_TARGET/$name"
-        if [ ! -d "$WATCH_TARGET" ]; then
-            param="$WATCH_TARGET"
-        fi
         if [ -x "$job" ]; then
             debug "INFO: starting job $job with param $param"
-            "$job" "$param"
+            "$job" "$name"
         else
             debug "INFO: can't execute $job"
         fi
